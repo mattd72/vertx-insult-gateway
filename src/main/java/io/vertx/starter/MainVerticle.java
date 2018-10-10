b8c6274a9f776ebc1416771b2806e0b8b548d766
mattd72 <mattd@redhat.com>
2018-10-10 19:01:06 -0400

    did 1-4

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,14 +1,43 @@
 package io.vertx.starter;
 
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+import io.reactivex.Maybe;
+import io.vertx.core.json.JsonObject;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.reactivex.config.ConfigRetriever;
+import io.vertx.core.DeploymentOptions;
+
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
-        .listen(8080);
-  }
+  public void start(Future<Void> startFuture) {
+
+    initConfigRetriever()
+      .doOnError(startFuture::fail)
+      .subscribe(ar -> {
+        vertx.deployVerticle(InsultGatewayVerticle.class.getName(), new DeploymentOptions().setConfig(ar));
+        startFuture.complete();
+      });
+    }
+
+  private Maybe<JsonObject> initConfigRetriever() {
+
+		// Load the default configuration from the classpath
+		ConfigStoreOptions localConfig = new ConfigStoreOptions()
+			.setType("file")
+			.setFormat("json")
+			.setConfig(new JsonObject().put("path", "insult-config.json"));
+
+		// Add the default and container config options into the ConfigRetriever
+		ConfigRetrieverOptions retrieverOptions = new ConfigRetrieverOptions()
+			.addStore(localConfig);
+
+		// Create the ConfigRetriever and return the Maybe when complete
+		return ConfigRetriever.create(vertx, retrieverOptions).rxGetConfig().toMaybe();
+	}
 
 }
--- src/test/java/io/vertx/starter/MainVerticleTest.java
+++ src/test/java/io/vertx/starter/MainVerticleTest.java
@@ -1,6 +1,10 @@
+
 package io.vertx.starter;
 
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -17,7 +21,18 @@ public class MainVerticleTest {
   @Before
   public void setUp(TestContext tc) {
     vertx = Vertx.vertx();
+
+    JsonObject localConfig=new JsonObject();
+    localConfig.put("gateway.host.springboot.noun", "thorntail-rest-http-thorntail-noun.b9ad.pro-us-east-1.openshiftapps.com");
+    localConfig.put("gateway.host.springboot.noun.port", 80);
+    localConfig.put("gateway.host.wildfly-swarm.adj", "spring-boot-rest-http-springboot-adj.b9ad.pro-us-east-1.openshiftapps.com");
+    localConfig.put("gateway.host.wildfly-swarm.adj.port", 80);
+    localConfig.put("gateway.host.vertx.adj", "spring-boot-rest-http-springboot-adj.b9ad.pro-us-east-1.openshiftapps.com");
+    localConfig.put("gateway.host.vertx.adj.port", 80);
+
+
     vertx.deployVerticle(MainVerticle.class.getName(), tc.asyncAssertSuccess());
+    vertx.deployVerticle(InsultGatewayVerticle.class.getName(),new DeploymentOptions().setConfig(localConfig), tc.asyncAssertSuccess());
   }
 
   @After
@@ -28,11 +43,11 @@ public class MainVerticleTest {
   @Test
   public void testThatTheServerIsStarted(TestContext tc) {
     Async async = tc.async();
-    vertx.createHttpClient().getNow(8080, "localhost", "/", response -> {
-      tc.assertEquals(response.statusCode(), 200);
+    vertx.createHttpClient().getNow(8080, "localhost", "/api/insult", response -> {
+
       response.bodyHandler(body -> {
-        tc.assertTrue(body.length() > 0);
-        tc.assertTrue(body.toString().equalsIgnoreCase("Hello Vert.x!"));
+    	tc.assertTrue(body.length() > 0);
+    	tc.assertTrue(body.toJsonObject().containsKey("noun"));
         async.complete();
       });
     });
